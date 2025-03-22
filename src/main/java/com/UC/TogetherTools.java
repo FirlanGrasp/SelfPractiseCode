4c50685a80cd31b5a6ef49c2860faa277a39ac11
FirlanGrasp <chenshan_email.foxmail.com>
2025-03-22 13:09:13 +0800

    你好

---
+++ src/main/java/com/UC/TogetherTools.java
@@ -0,0 +1,29 @@
+package com.UC;
+
+import java.util.concurrent.Exchanger;
+
+public class TogetherTools {
+    public static void main(String[] args) {
+        Exchanger<String> ex = new Exchanger<>();
+
+        new Thread(() -> {
+            try{
+                String say = "我是AAA";
+                String get = ex.exchange(say);
+                System.out.println(Thread.currentThread().getName() + "收到的信息是" + get);
+            }catch (Exception e){
+                throw new RuntimeException();
+            }
+        }, "A").start();
+
+        new Thread(() -> {
+            try{
+                String say = "我是BBB";
+                String get = ex.exchange(say);
+                System.out.println(Thread.currentThread().getName() + "收到的信息是" + get);
+            }catch (Exception e){
+                throw new RuntimeException();
+            }
+        }, "B").start();
+    }
+}
