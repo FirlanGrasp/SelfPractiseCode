fb125d8881b39d8ff88f5d25b0dba7d1261fe453
FirlanGrasp <chenshan_email.foxmail.com>
2025-03-22 16:38:23 +0800

    czh 和 exchanger 简单修改

--- src/main/java/com/UC/TogetherTools.java
+++ src/main/java/com/UC/TogetherTools.java
@@ -25,5 +25,25 @@ public class TogetherTools {
                 throw new RuntimeException();
             }
         }, "B").start();
+
+        new Thread(() -> {
+            try{
+                String say = "我是CCC";
+                String get = ex.exchange(say);
+                System.out.println(Thread.currentThread().getName() + "收到的信息是" + get);
+            }catch (Exception e){
+                throw new RuntimeException();
+            }
+        }, "C").start();
+
+        new Thread(() -> {
+            try{
+                String say = "我DDD";
+                String get = ex.exchange(say);
+                System.out.println(Thread.currentThread().getName() + "收到的信息是" + get);
+            }catch (Exception e){
+                throw new RuntimeException();
+            }
+        }, "d").start();
     }
 }
